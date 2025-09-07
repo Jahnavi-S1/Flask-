c9a54ce3578a5dcb6e6b56553b5e914b498a6452
Jyothi <jahnavis.1523@gmail.com>
2025-09-07 20:08:19 +0530

    new python file

---
+++ Flask.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+import hashlib, time
+
+app = Flask(__name__)
+
+store = []  # temporary in-memory storage
+
+@app.post("/ingest")
+def ingest():
+    data = request.get_data()
+    sha = hashlib.sha256(data).hexdigest()
+    store.append({
+        "ts": time.time(),
+        "raw": data.decode(),
+        "sha256": sha
+    })
+    return jsonify({"ok": True, "sha256": sha})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
